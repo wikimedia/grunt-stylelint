d86ff77958e5e92ea0bb0901116df2b9fc43e38f
Ed S <ejsanders@gmail.com>
2020-08-03 17:41:18 +0100

    Update dependencies (#92)
    Fix resulting lint errors

--- test/stylelint.test.js
+++ test/stylelint.test.js
@@ -1,13 +1,11 @@
-var grunt = require( 'grunt' );
+'use strict';
+
+const grunt = require( 'grunt' );
 
 exports.stylelint = {
 	outputFile: function ( test ) {
-		var actual, expected;
-
-		test.expect( 1 );
-
-		actual = grunt.file.read( 'tmp/outputFile/report.txt' );
-		expected = grunt.file.read( 'test/expected/outputFile/report.txt' );
+		const actual = grunt.file.read( 'tmp/outputFile/report.txt' );
+		const expected = grunt.file.read( 'test/expected/outputFile/report.txt' );
 
 		test.equal( actual, expected, 'Should report to file.' );
 		test.done();
